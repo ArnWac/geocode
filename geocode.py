c60bbea36142d6cebf33ffe6afcd0ba97b4454c7
ArnWac <arnewacker@outlook.com>
2020-11-17 13:10:36 +0100

    Initial commit

--- geocode.py
+++ geocode.py
@@ -5,7 +5,7 @@ api_key ='APY_KEY here'
 
 def google_v3(address, data_type = 'json'):
     endpoint = f"https://maps.googleapis.com/maps/api/geocode/{data_type}"
-    params = {"address": address, "key": api_key, "language": 'sv'}
+    params = {"address": address, "key": api_key, "language": 'de'}
     url_params = urlencode(params)
     url = f"{endpoint}?{url_params}"
     r = requests.get(url)
