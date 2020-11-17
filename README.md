3ec3b5653db2d2dcb13633861109a050eb4b38df
ArnWac <arnewacker@outlook.com>
2020-11-17 20:21:31 +0100

    Readme added

---
+++ README.md
@@ -0,0 +1,21 @@
+<p align="center">
+  <h3 align="center">README</h3>
+</p>
+
+<!-- TABLE OF CONTENTS -->
+## Table of Contents
+
+* [About the Project](#about-the-project)
+* [Contact](#contact)
+
+<!-- ABOUT GEOCODE -->
+## About The Project
+
+I wanted to experiment with the Google Maps API to extract a geocode location (latitude and longitude).
+
+<!-- CONTACT -->
+## Contact
+
+Arne Wacker - arnewacker@outlook.com
+
+GitHub Link: [https://github.com/ArnWac](https://github.com/ArnWac)
--- geocode.py
+++ geocode.py
@@ -3,18 +3,21 @@ import requests
 
 api_key ='APY_KEY here'
 
-def google_v3(address, data_type = 'json'):
-    endpoint = f"https://maps.googleapis.com/maps/api/geocode/{data_type}"
-    params = {"address": address, "key": api_key, "language": 'de'}
+class Error(Exception):
+    pass
+
+def gmaps(address, data_type = 'json'):
+    endpoint = f'https://maps.googleapis.com/maps/api/geocode/{data_type}'
+    params = {'address': address, 'key': api_key, 'language': 'de'}
     url_params = urlencode(params)
-    url = f"{endpoint}?{url_params}"
+    url = f'{endpoint}?{url_params}'
     r = requests.get(url)
     if r.status_code not in range(200, 299):
         return {}
-    latlng = {}
     try:
-        latlng = r.json()['results'][0]['geometry']['location']
+        results = r.json()['results'][0]['geometry']['location']
+        if results is not None:
+            return results.get('lat'), results.get('lng')
+        raise Error("No results for '%s'" % address)
     except:
         pass
-    return latlng.get('lat'), latlng.get('lng')
-
