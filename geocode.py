83d24734d31582a468142860b53171400f42dbd1
ArnWac <arnewacker@outlook.com>
2020-11-17 13:08:12 +0100

    Initial commit

---
+++ geocode.py
@@ -0,0 +1,20 @@
+from urllib.parse import urlencode
+import requests
+
+api_key ='APY_KEY here'
+
+def google_v3(address, data_type = 'json'):
+    endpoint = f"https://maps.googleapis.com/maps/api/geocode/{data_type}"
+    params = {"address": address, "key": api_key, "language": 'sv'}
+    url_params = urlencode(params)
+    url = f"{endpoint}?{url_params}"
+    r = requests.get(url)
+    if r.status_code not in range(200, 299):
+        return {}
+    latlng = {}
+    try:
+        latlng = r.json()['results'][0]['geometry']['location']
+    except:
+        pass
+    return latlng.get('lat'), latlng.get('lng')
+
